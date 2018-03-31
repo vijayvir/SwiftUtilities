44ac92abd48f7e91cb1b3c331000f3c0d9a3c33a
vijayvir singh <virvijay37@gmail.com>
2018-03-31 17:18:10 +0530

    XML
    XML

--- CountryCodes/CountryCodes/ViewController.swift
+++ CountryCodes/CountryCodes/ViewController.swift
@@ -31,4 +31,78 @@ class ViewController: UIViewController {
 
 
 }
+/*
+import Foundation
+import SWXMLHash
+struct SoapAddNewPassengerRequestIncomming{
+    var addNewPassengerRequestResult: String?
+}
+class SoapAddNewPassengerRequestOutcomming{
+    var soapAddNewPassengerRequestIncomming = SoapAddNewPassengerRequestIncomming()
+    var closureAddNewPassengerRequestResponse: ((SoapAddNewPassengerRequestIncomming) -> Void)?
+    // 24 parameters to passed while AddNewPassengerRequest Api is called
+     func request(userPKUID: String ,
+                 pickUpAddress: String,
+                 pickUpCity: String,
+                 pickUpState: String,
+                 pickUpZip: String,
+                 pickUpZipPlusFour: String,
+                 pickUpLatitude: String,
+                 pickUpLongitude: String,
+                 pickUpDescription: String,
+                 pickUpDate: String,
+                 pickUpTime: String,
+                 dropOffAddress: String,
+                 dropOffCity: String,
+                 dropOffState: String,
+                 dropOffZip: String,
+                 dropOffLatitude: String,
+                 dropOffLongitude: String,
+                 dropOffZipPlusFour: String,
+                 dropOffDescription: String,
+                 dropOffDate: String,
+                 dropOffTime: String,
+                 piecesOfLuggage: String,
+                 luggageDescription: String,
+                 paymentMethod: String,
+                meetUpRadius: String   ){
+        
+        let headers = [
+            "content-type": "text/xml; charset=utf-8",
+            "soapaction": EnumSoapActions.addNewPassengerRequest.name,
+            "cache-control": "no-cache",
+            ]
+       //24 parameters
+        let postData = NSData(data: "<?xml version='1.0' encoding='utf-8'?><soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'><soap:Body><AddNewPassengerRequest xmlns='http://SlideRide.com/'><UserPKUID>\(userPKUID)</UserPKUID><PickUpAddress>\(pickUpAddress)</PickUpAddress><PickUpCity>\(pickUpCity)</PickUpCity><PickUpState>\(pickUpState)</PickUpState><PickUpZip>\(pickUpZip)</PickUpZip><PickUpZipPlusFour>\(pickUpZipPlusFour)</PickUpZipPlusFour><PickUpLatitude>\(pickUpLatitude)</PickUpLatitude><PickUpLongitude>\(pickUpLongitude)</PickUpLongitude><PickUpDescription>\(pickUpDescription)</PickUpDescription><PickUpDate>\(pickUpDate)</PickUpDate><PickUpTime>\(pickUpTime)</PickUpTime><DropOffAddress>\(dropOffAddress)</DropOffAddress><DropOffCity>\(dropOffCity)</DropOffCity><DropOffState>\(dropOffState)</DropOffState><DropOffZip>\(dropOffZip)</DropOffZip><DropOffLatitude>\(dropOffLatitude)</DropOffLatitude><DropOffLongitude>\(dropOffLongitude)</DropOffLongitude><DropOffZipPlusFour>\(dropOffZipPlusFour)</DropOffZipPlusFour><DropOffDescription>\(dropOffDescription)</DropOffDescription><DropOffDate>\(dropOffDate)</DropOffDate><DropOffTime>\(dropOffTime)</DropOffTime><PiecesOfLuggage>\(piecesOfLuggage)</PiecesOfLuggage><LuggageDescription>\(luggageDescription)</LuggageDescription><PaymentMethod>\(paymentMethod)</PaymentMethod><MeetUpRadius>\(meetUpRadius)</MeetUpRadius></AddNewPassengerRequest></soap:Body></soap:Envelope>".data(using: String.Encoding.utf8)!)
 
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "http://graysonshare.cgmcc.com/webservice/handhelddataaccess.asmx")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = postData as Data
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse!)
+                let str = String.init(data: data!, encoding: .utf8)
+                let xml = SWXMLHash.parse(str!)
+                print("👤",xml)
+                let userResponse = xml["soap:Envelope"]["soap:Body"]["AddNewPassengerRequestResponse"]["AddNewPassengerRequestResult"].element?.text
+                print("🧕🏻 ",userResponse.leoSafe())
+              
+            }
+        })
+        
+        dataTask.resume()
+        
+    }
+    
+    
+}
+*/
